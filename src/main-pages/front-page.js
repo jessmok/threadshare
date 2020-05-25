4c805124c1f289ed536dfc91901104e97ba21925
eparella <ericparella@gmail.com>
2020-05-24 21:40:41 -0700

    https://github.com/eparella/Spring307-8amMWF-Team9/issues/38

--- src/main-pages/front-page.js
+++ src/main-pages/front-page.js
@@ -3,24 +3,10 @@ import fetch from 'isomorphic-fetch';
 import '../firststyle.css';
 import DisplayPage, { ThisMatters } from '../components/display-page';
 import ThreadCreator from '../components/thread-creator';
+import ReplyCreator from '../components/reply-creator';
 
 export default function FrontPage({}) {
     const [threads, setThreads] = useState({ count: 0, threads: [] });
-    /*useEffect(() => {
-        console.log('running use effect');
-        const getData = async () => {
-            const x = await fetch(
-                'http://data.nba.net/prod/v1/20191025/scoreboard.json',
-                {
-                    method: 'GET',
-                    headers: { 'Access-Control-Allow-Origin': '*' },
-                },
-            );
-        };
-        getData();
-    }, []);*/
-
-    console.log('rendering');
     return (
         <div>
             <h1>LATEST THREADS</h1>
@@ -28,6 +14,16 @@ export default function FrontPage({}) {
                 return <DisplayPage thread={t} key={t.title} />;
             })}
             <ThreadCreator
+                onSubmit={(newThread) => {
+                    console.log('Submitted', newThread);
+                    setThreads({
+                        count: threads.count + 1,
+                        threads: [...threads.threads, newThread],
+                    });
+                }}
+            />
+
+            <ReplyCreator
                 onSubmit={(newThread) => {
                     console.log('Submitted', newThread);
                     setThreads({
