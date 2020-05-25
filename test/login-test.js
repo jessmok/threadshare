c1e8cdccd7748ed9c63b568fc2d3eedd94cd3936
Jessie Mok <jessmok108@yahoo.com>
2020-05-24 19:23:03 -0700

    Added and fixed sign out button https://github.com/eparella/Spring307-8amMWF-Team9/issues/32

---
+++ test/login-test.js
@@ -0,0 +1,35 @@
+const test = require('firebase-functions-test')();
+const functions = require('../index.html');
+
+describe('handleSignUp', () => {
+  it('successfully invokes function', async () => {
+    const wrapped = test.wrap(functions.handleSignUp);
+    const data = { email: 'test-email@gmail.com', password: "test-password" }
+    await wrapped({
+      data: () => ({
+        name: 'test-user'
+      }),
+      ref:{
+        set: jest.fn()
+      }
+    })
+  })
+})
+
+const mockQueryResponse = jest.fn()
+mockQueryResponse.mockResolvedValue([
+  {
+    id: 1
+  }
+])
+
+jest.mock('firebase-admin', () => ({
+  initializeApp: jest.fn(),
+  firestore: () => ({
+   collection: jest.fn(path => ({
+     where: jest.fn(queryString => ({
+       get: mockQueryResponse
+     }))
+   })) 
+  })
+}))
