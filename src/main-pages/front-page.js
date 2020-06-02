6035c8e52654a3ca0f743f8a5a79f14b7ffbe853
eparella <ericparella@gmail.com>
2020-06-01 23:36:50 -0700

    Major login changes.

--- src/main-pages/front-page.js
+++ src/main-pages/front-page.js
@@ -4,12 +4,20 @@ import '../firststyle.css';
 import DisplayPage, { ThisMatters } from '../components/display-page';
 import ThreadCreator from '../components/thread-creator';
 import ReplyCreator from '../components/reply-creator';
-
+import Login from '../components/login';
+const firebaseApp = window.firebaseApp;
 export default function FrontPage({}) {
     const [threads, setThreads] = useState({
         count: 0,
         threads: [],
     });
+
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    const signIn = ({ user }) => {
+        const { email, uid } = user;
+        setLoggedIn({ email, uid });
+    };
 
     const addReply = ({ index, reply }) => {
         threads.threads
@@ -22,25 +30,31 @@ export default function FrontPage({}) {
 
     return (
         <div>
-            <h1>LATEST THREADS</h1>
-            {threads.threads.map((t) => {
-                return (
-                    <DisplayPage
-                        thread={t}
-                        key={t.title}
-                        addReply={addReply}
-                        replies={t.replies}
+            {!loggedIn ? (
+                <Login onSignIn={signIn} />
+            ) : (
+                <>
+                    <h1>LATEST THREADS</h1>
+                    {threads.threads.map((t) => {
+                        return (
+                            <DisplayPage
+                                thread={t}
+                                key={t.title}
+                                addReply={addReply}
+                                replies={t.replies}
+                            />
+                        );
+                    })}
+                    <ThreadCreator
+                        onSubmit={(newThread) => {
+                            setThreads({
+                                count: threads.count + 1,
+                                threads: [...threads.threads, newThread],
+                            });
+                        }}
                     />
-                );
-            })}
-            <ThreadCreator
-                onSubmit={(newThread) => {
-                    setThreads({
-                        count: threads.count + 1,
-                        threads: [...threads.threads, newThread],
-                    });
-                }}
-            />
+                </>
+            )}
         </div>
     );
 }
